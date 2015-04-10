38f87a7ccba6c35234edc9695960417315e644b3
jborowitz <jeffrey.borowitz@giantoak.com>
2015-04-10 21:55:55 +0000

    API: Change dependency to deepdive data; use R package data for app
    The changes to the data files attempt to back the app with data from the
    February/March DeepDive extractions. The changes to the code reflect
    the fact that we now need to use a different set of column names for this
    data, and that we call the version of the endpoint in get_features.R from views.py

--- app/views.py
+++ app/views.py
@@ -26,21 +26,13 @@ import sys
 @app.route('/get_comparison_upload/', methods=['POST'])
 def get_comparison_upload():
     start = datetime.datetime.now()
-
-    with open(os.path.join(APP_ROOT, 'static', 'data', 'region_features.csv')) as f:
-        files = {'data': f.read()}
-    endpoint = 'ocpu/library/rlines/R/store_csv/'
-    d = ocpu_wrapper(url=endpoint,  files=files)
-    d.perform()
-    
     input_data = json.loads(request.data)
     data = {
-            'target.region':str(input_data['targetRegion']) ,
-            'comparison.vars':['completeness','b01001001','counts'],
-            'input_data':d,
+            'target.region':str(input_data['targetRegion']),
+            'comparison.vars':['population','unemployment','female_wage_p50','male_wage_p50'],
             }# Convert input data from unicode
     header = { 'content-type': 'application/x-www-form-urlencoded' } # Set header for ocpu
-    endpoint = 'ocpu/library/rlines/R/get_features_data/'
+    endpoint = 'ocpu/library/rlines/R/get_features/'
     print 'About to create ocpu object in %s' % str(datetime.datetime.now() - start)
     d = ocpu_wrapper(url=endpoint, data=dict_to_r_args(data), header=header)
     print ' ocpu object created in %s' % str(datetime.datetime.now() - start)
--- opencpu-app/rlines/R/get_features.R
+++ opencpu-app/rlines/R/get_features.R
@@ -7,10 +7,11 @@
 #' - features as columns
 #' - score as a final column measuring similarity
 
-get_features<-function(target.region, num.matches=4){
-  data<-region.features[,c('completeness','b01001001','counts','region')] # Select columns to use
+get_features<-function(target.region, num.matches=4, region.var='region', comparison.vars){
+  data<-region.features[,c(comparison.vars, region.var)] # Select columns to use
   data$target <- data$region == target.region # Create a binary 1 for the region we care about
-  match<-matchit(target ~ completeness + b01001001 + counts, data=data)
+  matching.formula<-paste(target.varname,'~',paste(comparison.vars,collapse=' + '))
+  match<-matchit(as.formula(matching.formula), data=data)
   data$score <- match$distance
   target.row <- data[data$target,]
   data<-data[!data$target,]
